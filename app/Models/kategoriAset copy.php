208891825bad80c24a3ff65f0087816aa4fe11bb
Duncan <richardpratama9898@gmail.com>
2025-07-17 10:20:08 +0700

    Initial commit

---
+++ app/Models/kategoriAset copy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kategoriAset extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql_hris';
+
+    protected $table = 'kategori_aset';
+
+    protected $fillable = [
+        'kode_kategori_aset',
+        'nama_kategori_aset',
+
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo(Aset::class, 'kode_kategori_aset', 'kategori_aset');
+    }
+}
